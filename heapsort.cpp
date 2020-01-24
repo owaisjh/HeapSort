49d5d4f4f916ad9fea92e5ee068a3e8a7194b4d6
owaisjh <50790949+owaisjh@users.noreply.github.com>
2020-01-24 16:17:50 +0530

    Add files via upload

---
+++ heapsort.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+
+void heapsort(int x[], int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		int c=i;
+		int temp;
+		do
+		{
+			int r=(c-1)/2;
+			if(x[c]>x[r])
+			{
+				temp=x[r];
+				x[r]=x[c];
+				x[c]=temp;
+			}
+			c=r;
+		}while(c!=0);
+		
+		temp=x[0];
+		x[0]=x[size-1];
+		x[size-1]=temp;
+	}
+	if(size>1)
+		heapsort(x,size-1);
+}
+
+
+int main()
+{
+	int size;
+	
+
+	cout<<"\nEnter Size:";
+	cin>>size;
+	int heap[size];
+	cout<<"Enter elements:\n";
+	for(int i=0;i<size;i++)
+	{
+		cin>>heap[i];
+	}
+	heapsort(heap,size);
+
+	cout<<"\nSorted:\n";
+	for(int i=0;i<size;i++)
+	{
+		cout<<heap[i]<<" ";
+	}
+	return 0;
+}
+
+
+mail kar heap sort,im sleeping, bye thanks love you too
+	for(int i=0;i<size;i++)
+	{
+		int c=i;
+		do
+		{
+			int r= (c-1)/2;
+			if(x[c]>x[r])
+			{
+				int temp= x[r];
+				x[r]=x[c];
+				x[c]=temp;
+			}
+			c=r;
+		}while(c!=0);
+	}
+
+	int temp=x[0];
+	x[0]=x[size-1];
+	x[size-1]=temp;
+	
+	if(size>1)
+		heapsort(x,size-1);
